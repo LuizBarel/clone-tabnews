bebd74c688d3f3a79f59a0d84de24898d77cda19
Luiz Ricardo Barel <138068450+LuizBarel@users.noreply.github.com>
2024-11-02 19:02:22 +0000

    feat: Criação da página index

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+// Sintaxe para componentes React: 1ª letra de cada palavra maiuscula
+// Todo componente React se escreve igual uma função JS
+
+
+/**
+ *  Componente React para a Home
+ */ 
+function Home() {
+    return <h1>Teste</h1>
+}
+
+// Para saber qual função o Next deve retornar para renderizar a página, é necessário utilizar esta linha:
+export default Home; // export (exportar para fora do arquivo) default (é para definir o padrão do arquivo) NomeDaFunção
