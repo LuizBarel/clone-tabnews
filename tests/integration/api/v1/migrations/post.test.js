d464e0debd8144b2071fdd25a0b4c5ea08dc4655
Luiz Ricardo Barel <138068450+LuizBarel@users.noreply.github.com>
2025-01-04 20:49:29 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -6,31 +6,37 @@ beforeAll(async () => {
   await database.cleanDatabase();
 });
 
-/**
- * Explicação do teste:
- * Ele vai simular 2 requisições POST em sequência.
- * A primeira requsição sobre a rota deve executar as migrations, por isso deve retornar o status code 201 (created), e junto disso, um array com as migrations que foram executadas.
- * Na segunda requisição, a rota deve tentar executar novamente as migrations, mas ela irá ver que não existe mais migrations a ser rodadas, e por isso, deve retornar um status
- * code 200 (ok) e um array vazio.
- */
-test("POST to /api/v1/migrations should return 200", async () => {
-  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymus user", () => {
+    describe("Running pending migrations", () => {
+      test("For the first time", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response.status).toBe(201);
+
+        const responseBody = await response.json();
+
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBeGreaterThan(0);
+      });
+      test("For the second time time", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response.status).toBe(200);
+
+        const responseBody = await response.json();
+
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBe(0);
+      });
+    });
   });
-  expect(response1.status).toBe(201);
-
-  const response1Body = await response1.json();
-
-  expect(Array.isArray(response1Body)).toBe(true);
-  expect(response1Body.length).toBeGreaterThan(0);
-
-  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
-  expect(response2.status).toBe(200);
-
-  const response2Body = await response2.json();
-
-  expect(Array.isArray(response2Body)).toBe(true);
-  expect(response2Body.length).toBe(0);
 });
