8cf006b5e71d2fbe0cfeaea775f159658c0d8580
Luiz Ricardo Barel <138068450+LuizBarel@users.noreply.github.com>
2025-03-24 01:51:39 +0000

    refactor: use `migrator.js` model on `/migrations/index.js`

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,43 +1,15 @@
 // Importações
-import migrationRunner from "node-pg-migrate";
-import { resolve } from "node:path";
-import database from "infra/database.js";
 import { createRouter } from "next-connect";
 import controller from "infra/controller";
+import migrator from "models/migrator";
 
 // Criação da rota
 const router = createRouter();
 
-router.use(globalHandler);
 router.get(getHandler);
 router.post(postHandler);
 
 export default router.handler(controller.errorHandlers);
-
-/**
- * Função que vai ser executada sempre que uma requisição for solicitada
- */
-async function globalHandler(request, response, next) {
-  let dbClient;
-
-  try {
-    dbClient = await database.getNewClient();
-
-    request.defaultMigrationOptions = {
-      dbClient: dbClient,
-      dryRun: true,
-      dir: resolve("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pg-migrations",
-    };
-
-    // Executa a próxima função (no caso, a função handler do método HTTP usado)
-    await next();
-  } finally {
-    dbClient.end();
-  }
-}
 
 /**
  * Função que verifica as migrations pendentes do sistema
@@ -45,10 +17,7 @@ async function globalHandler(request, response, next) {
  * @param {*} response parametro da resposta que vai ser retornada
  */
 async function getHandler(request, response) {
-  const pendingMigrations = await migrationRunner(
-    request.defaultMigrationOptions,
-  );
-
+  const pendingMigrations = await migrator.listPendingMigrations();
   return response.status(200).json(pendingMigrations);
 }
 
@@ -58,11 +27,7 @@ async function getHandler(request, response) {
  * @param {*} response parametro da resposta que vai ser retornada
  */
 async function postHandler(request, response) {
-  const migratedMigrations = await migrationRunner({
-    ...request.defaultMigrationOptions, // ... -> spread: espalha os atributos do objeto dentro de outro, permitindo a reescrita de quaisquer atributos
-
-    dryRun: false,
-  });
+  const migratedMigrations = await migrator.runPendingMigrations();
 
   if (migratedMigrations.length > 0) {
     return response.status(201).json(migratedMigrations);
