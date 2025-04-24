67878da758c2f2ddb3b0f217da2045b1fc06524a
Luiz Ricardo Barel <138068450+LuizBarel@users.noreply.github.com>
2025-04-24 19:06:01 +0000

    feat: create endpoint `/api/v1/users` and `user` model

---
+++ models/user.js
@@ -0,0 +1,76 @@
+import database from "infra/database";
+import { ValidationError } from "infra/errors";
+
+async function create(userInputValues) {
+  await validateUniqueEmail(userInputValues.email);
+  await validateUniqueUsername(userInputValues.username);
+
+  const newUser = await runInsertQuery(userInputValues);
+  return newUser;
+
+  async function runInsertQuery(userInputValues) {
+    const results = await database.query({
+      text: `
+        INSERT INTO 
+          users (username, email, password) 
+        VALUES 
+          ($1, $2, $3)
+        RETURNING
+          *
+        ;`,
+      values: [
+        userInputValues.username,
+        userInputValues.email,
+        userInputValues.password,
+      ],
+    });
+
+    return results.rows[0];
+  }
+
+  async function validateUniqueEmail(email) {
+    const results = await database.query({
+      text: `
+        SELECT
+          email
+        FROM
+          users
+        WHERE
+          LOWER(email) = LOWER($1)
+        ;`,
+      values: [email],
+    });
+
+    if (results.rowCount > 0)
+      throw new ValidationError({
+        message: "O e-mail informado já está sendo utilizado.",
+        action: "Use outro e-mail para realizar o cadastro",
+      });
+  }
+
+  async function validateUniqueUsername(username) {
+    const results = await database.query({
+      text: `
+        SELECT
+          username
+        FROM
+          users
+        WHERE
+          LOWER(username) = LOWER($1)
+        ;`,
+      values: [username],
+    });
+
+    if (results.rowCount > 0)
+      throw new ValidationError({
+        message: "O nome informado já está sendo utilizado.",
+        action: "Use outro nome para realizar o cadastro",
+      });
+  }
+}
+
+const user = {
+  create,
+};
+
+export default user;
